f3d099c7dc17800417aea9cc25ea4bce9dd3bed4
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-02-23 22:12:27 -0300

    refactor: update home view to get header component

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <main class="container -home">
+    <Header msg="Welcome to Your Vue.js + TypeScript App" />
+  </main>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Header from '@/components/header/index.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    Header,
   },
 })
 export default class Home extends Vue {}
