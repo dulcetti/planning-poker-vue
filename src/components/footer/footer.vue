06fda5640019fe4d6666196dc112711e4d27133c
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-02-27 18:38:46 -0300

    feat: add initial footer

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer"></footer>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {},
+})
+export default class Footer extends Vue {}
+</script>
+
+<style scoped lang="scss" src="./styles.scss"></style>
