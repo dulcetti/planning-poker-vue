62a409b1d2a3bc6d0cf6abc2bb4d83d05f539a6d
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-03-10 19:52:55 -0300

    feat(card-list): component and props - add section, title and list of cards - add card component with bind value from state - add Card component on Options - add computed with values of state on Options - add styles tag

---
+++ src/components/cards-list/cards-list.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="wrapper-cards">
+    <h1 class="title">Choose your Card:</h1>
+    <ul class="cards-list">
+      <li v-for="value in values" v-bind:key="value">
+        <Card v-bind:value="value" />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import createStore from '@/store';
+import Card from '@/components/card/card.vue';
+
+@Options({
+  components: {
+    Card,
+  },
+  computed: {
+    values() {
+      return createStore.state.cardValues;
+    },
+  },
+})
+export default class CardsList extends Vue {}
+</script>
+
+<style scoped lang="scss" src="./styles.scss"></style>
