71a3b7484512f6234b3cd97795298d77642c368e
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-03-17 21:50:14 -0300

    feat: click on card - add on click to execute chooseCard event - add chooseCard method to update the selected card

--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="card">
+  <div class="card" v-on:click="chooseCard(value)">
     {{ value }}
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import createStore from '@/store';
 
 @Options({
   props: {
     value: Number,
+  },
+  methods: {
+    chooseCard: (value: number) => {
+      createStore.state.selectedCard = value;
+    },
   },
 })
 export default class Card extends Vue {
