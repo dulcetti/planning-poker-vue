b37146d6ae2e392509aa957aadf0dcdb5206ad35
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-03-24 20:53:31 -0300

    refactor: remove your card text from play page

--- src/views/play.vue
+++ src/views/play.vue
@@ -1,24 +1,17 @@
 <template>
   <main class="container -play">
     <CardsList />
-    Your card: <span v-if="selectedCard > 0">{{ selectedCard }}</span>
   </main>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 
-import createStore from '@/store';
 import CardsList from '@/components/cards-list/cards-list.vue';
 
 @Options({
   components: {
     CardsList,
-  },
-  computed: {
-    selectedCard() {
-      return createStore.state.selectedCard;
-    },
   },
 })
 export default class Play extends Vue {}
