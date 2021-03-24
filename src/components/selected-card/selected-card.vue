7ee6175b9f4b0d2a25cb9cc420834cf65d2ee4ac
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-03-24 20:50:11 -0300

    feat(selected-card): add initial component

---
+++ src/components/selected-card/selected-card.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="selected-card"></div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {},
+})
+export default class SelectedCard extends Vue {}
+</script>
+
+<style scoped lang="scss" src="./styles.scss"></style>
