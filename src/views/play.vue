c16d33ad4e43e918e2cd616db1a373bfe178255e
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-03-24 21:09:35 -0300

    feat: add selected card component on play page

--- src/views/play.vue
+++ src/views/play.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="container -play">
     <CardsList />
+    <SelectedCard />
   </main>
 </template>
 
@@ -8,10 +9,12 @@
 import { Options, Vue } from 'vue-class-component';
 
 import CardsList from '@/components/cards-list/cards-list.vue';
+import SelectedCard from '@/components/selected-card/selected-card.vue';
 
 @Options({
   components: {
     CardsList,
+    SelectedCard,
   },
 })
 export default class Play extends Vue {}
