221c75ab04715f7acf26db4e07c60f0a62ab2916
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-03-26 19:35:55 -0300

    test(selected-card): add imports and first desc

---
+++ src/components/selected-card/spec.ts
@@ -0,0 +1,5 @@
+import { shallowMount } from '@vue/test-utils';
+import SelectedCard from '@/components/selected-card/selected-card.vue';
+import Card from '@/components/card/card.vue';
+
+describe('<SelectedCard />', () => {});
