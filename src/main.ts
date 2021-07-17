9b9879b41fab417e09648e69fee2aa80facd5a28
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-07-16 22:46:58 -0300

    feat: add element plus on main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app');
